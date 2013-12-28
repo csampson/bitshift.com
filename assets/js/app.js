87c3d4a746a94a4b1dd57246c986dccb8b9fafca
Cameron Sampson <vaerros@gmail.com>
2013-12-27 18:30:25 -0600

    Fixing some indentation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
 $('.check-us-out').on('click', function() {
-	$('html, body').animate({ scrollTop: $('.how-we-roll').offset().top + 8 });
+  $('html, body').animate({ scrollTop: $('.how-we-roll').offset().top + 8 });
 });
 
 $('.get-in-touch .minimal-logo').on('click', function() {
-	$('html, body').animate({ scrollTop: 0 });
+  $('html, body').animate({ scrollTop: 0 });
 });
 
 if($('.contact-form-errors').length) {
-	$('html, body').animate({ scrollTop: $('.contact-form-errors').offset().top });
+  $('html, body').animate({ scrollTop: $('.contact-form-errors').offset().top });
 }
